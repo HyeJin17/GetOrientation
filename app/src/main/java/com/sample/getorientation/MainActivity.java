a6e71463a77f0550a5922eb933db6ee2df1f29bc
HyeJin <rkd2313@gmail.com>
2021-04-14 15:05:11 +0900

    Orientation OnPause and OnResume

--- app/src/main/java/com/sample/getorientation/MainActivity.java
+++ app/src/main/java/com/sample/getorientation/MainActivity.java
@@ -30,18 +30,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        sensorManager.unregisterListener(listener);
-       // sensorManager.requestTriggerSensor( ,listener);
+        sensorManager.registerListener(listener, magSensor, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(listener, accSensor, SensorManager.SENSOR_DELAY_UI);
     }
 
     @Override
